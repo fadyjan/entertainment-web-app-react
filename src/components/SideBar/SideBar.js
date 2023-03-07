f547092e1b5785c48f69899b050f9a3e9892d193
fadyjan <fady.jan95@gmail.com>
2023-03-07 17:07:15 +0200

    Create #3 Create Sidebar component

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,35 @@
+import React,{useContext} from "react";
+import "./SideBar.css";
+
+import BookMarkIcon from "../../assets/icon-nav-bookmark.svg";
+import HomeIcon from "../../assets/icon-nav-home.svg";
+import MoviesIcon from "../../assets/icon-nav-movies.svg";
+import TvSeriesIcon from "../../assets/icon-nav-tv-series.svg";
+import LogoIcon from "../../assets/logo.svg";
+import {SelectedCategory} from "../../App"
+
+const SideBar = () => {
+ const SelectedCategoryContext =  useContext(SelectedCategory)
+ const HandleColorChange = (e)=>{
+  const Icons = Array.from(document.getElementById("SideBarMenuWrapper").getElementsByTagName("img"))
+  Icons.forEach((icon)=>{
+    icon.className = ""
+  })
+  e.target.className = "ActiveIcon"
+ }
+  return (
+    <div id="SideBar">
+      <div id="LogoWrapper">
+        <img src={LogoIcon} alt=""></img>
+      </div>
+      <div id="SideBarMenuWrapper">
+        <img className="ActiveIcon" src={HomeIcon} onClick={(e)=>{SelectedCategoryContext.CategoryHandler("Recommended for you");HandleColorChange(e)}} alt=""></img>
+        <img src={MoviesIcon}  onClick={(e)=>{SelectedCategoryContext.CategoryHandler("Movies");HandleColorChange(e)}}alt=""></img>
+        <img src={TvSeriesIcon} onClick={(e)=>{SelectedCategoryContext.CategoryHandler("Tv Series");HandleColorChange(e)}}alt=""></img>
+        <img src={BookMarkIcon}  onClick={(e)=>{SelectedCategoryContext.CategoryHandler("Bookmarked Movies");HandleColorChange(e)}}alt=""></img>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
