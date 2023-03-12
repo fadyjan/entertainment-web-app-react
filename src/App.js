0e282dd864f332864341e4e506c8e585fd780317
fadyjan <fady.jan95@gmail.com>
2023-03-12 21:25:19 +0200

    Create #10 App FinalTouches

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext } from "react";
+import OriginalData from "./data/data.json";
+import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
+import SearchBar from "./components/SearchBar/SearchBar";
+import SelectedSection from "./components/SelectedSection/SelectedSection";
+import Grid from "./components/Grid/Grid";
+
+export const DataContext = createContext(OriginalData);
+export const SelectedCategory = createContext(null);
 
 function App() {
+
+  const [currentSelected, setCurrentSelected] = useState('Recommended for you')
+  const [data,setData] = useState(OriginalData)
+
+  const CategoryHandler = (CurrentSelected)=>{
+    setCurrentSelected(CurrentSelected)
+  }
+
+  const SetDataHandler = (CurrentData)=>{
+    setData(CurrentData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataContext.Provider value={{ data ,OriginalData ,SetDataHandler}}>
+      <SelectedCategory.Provider value={{ currentSelected ,CategoryHandler}}>
+        <div className="App">
+          <div id="InnerAppWrapper">
+            <SideBar></SideBar>
+            <div id="MoviesContainer">
+              <div id="InnerMoviesContainer">
+                <SearchBar></SearchBar>
+                {currentSelected ==="Recommended for you" ? <SelectedSection></SelectedSection>:<></>}
+                <Grid></Grid>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SelectedCategory.Provider>
+    </DataContext.Provider>
   );
 }
 
